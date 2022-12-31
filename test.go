c239562ab39aed1f8d48068f613a560a63fd2ab7
younes taouil <younestaouil364@gmail.com>
2022-12-31 02:45:48 +0100

    add test files

---
+++ test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestTerraformAwsHelloWorldExample(t *testing.T) {
+	t.Parallel()
+
+	// retryable errors in terraform testing.
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../",
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	publicIp := terraform.Output(t, terraformOptions, "public_ip")
+
+	url := fmt.Sprintf("http://%s:8080", publicIp)
+	http_helper.HttpGetWithRetry(t, url, nil, 200, "Hello, World!", 30, 5*time.Second)
+}

9963c2e41c68a2327ec100644892fa8b236dc506
younes taouil <younestaouil364@gmail.com>
2022-12-31 12:32:46 +0100

    push

--- test.go
+++ test.go
@@ -1,59 +1,29 @@
-package aws
+package test
 
 import (
 	"fmt"
+	"testing"
+	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
+
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	"github.com/gruntwork-io/terratest/modules/testing"
 )
 
-// import (
-// 	"fmt"
-// 	"testing"
-
-// 	"github.com/gruntwork-io/terratest/modules/aws"
-// 	"github.com/gruntwork-io/terratest/modules/terraform"
-// 	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
-// )
-
-func TestCreateEc2Instance(t *testing.T) {
+func TestTerraformAwsHelloWorldExample(t *testing.T) {
 	t.Parallel()
 
-	// Load the test environment variables
-	test_structure.LoadIntegrationTestOptions(t, "test-fixture")
+	// retryable errors in terraform testing.
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../examples/terraform-aws-hello-world-example",
+	})
 
-	// Create a Terraform options struct
-	terraformOptions := &terraform.Options{
-		// The path to where our Terraform code is located
-		TerraformDir: "../auto-test",
-
-		// Variables to pass to our Terraform code using -var options
-		Vars: map[string]interface{}{
-			"instance_type": "t2.micro",
-		},
-	}
-
-	// At the end of the test, run `terraform destroy` to clean up any resources that were created
 	defer terraform.Destroy(t, terraformOptions)
 
-	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
 	terraform.InitAndApply(t, terraformOptions)
 
-	// Look up the ID of the EC2 instance created by the Terraform module
-	instanceID := terraform.Output(t, terraformOptions, "instance_id")
+	publicIp := terraform.Output(t, terraformOptions, "public_ip")
 
-	// Check that the instance is running
-	instance := aws.GetEc2InstanceById(t, instanceID)
-	if instance.State.Name != "running" {
-		t.Fatalf("Expected instance to be running, got %s", instance.State.Name)
-	}
-
-	// Check that the instance has the expected instance type
-	expectedInstanceType := terraformOptions.Vars["instance_type"].(string)
-	if instance.InstanceType != expectedInstanceType {
-		t.Fatalf("Expected instance to be %s, got %s", expectedInstanceType, instance.InstanceType)
-	}
-
-	fmt.Println("EC2 instance created successfully")
+	url := fmt.Sprintf("http://%s:8080", publicIp)
+	http_helper.HttpGetWithRetry(t, url, nil, 200, "Hello, World!", 30, 5*time.Second)
 }

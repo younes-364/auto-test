17fdd8a8829088c9eb28e6f62d9f99911c20ecaa
younes taouil <younestaouil364@gmail.com>
2022-12-31 12:34:03 +0100

    push

--- test.go
+++ test.go
@@ -15,7 +15,7 @@ func TestTerraformAwsHelloWorldExample(t *testing.T) {
 
 	// retryable errors in terraform testing.
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: "../examples/terraform-aws-hello-world-example",
+		TerraformDir: "../auto-test",
 	})
 
 	defer terraform.Destroy(t, terraformOptions)

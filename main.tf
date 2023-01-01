a33fc07ace6de5628be816171b6ee8560613bf87
younes-364 <younestaouil364@gmail.com>
2023-01-01 17:20:53 +0000

    push11

--- main.tf
+++ main.tf
@@ -4,8 +4,6 @@ resource "aws_vpc" "my_vpc" {
     Name = "tf-example"
   }
 }
-
-
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "172.16.10.0/24"

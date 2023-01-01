dcb62a88fa8f394648d87ca5d68668ecb684b8d3
younes-364 <younestaouil364@gmail.com>
2023-01-01 16:32:57 +0000

    push5

--- provider.tf
+++ provider.tf
@@ -1,8 +1,5 @@
 terraform {
-  provider "tfe" {
-    hostname = "app.terraform.io"
-    token = "eM6P0JyKaZzJaA.atlasv1.U0oMslxMeZtNlTMM5owqnPg0MomT8GIHa2GhyrTNl0zgE7vzksFfzCqHF2Wjzyuaflw"
-  }
+
   cloud {
     organization = "My-Organization394"
 
@@ -22,4 +19,8 @@ provider "aws" {
   access_key = "AKIAVPGBT54OGVNNC3XX"
   secret_key = "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"
   region     = "eu-central-1"
+}
+provider "tfe" {
+  hostname = "app.terraform.io"
+  token = "eM6P0JyKaZzJaA.atlasv1.U0oMslxMeZtNlTMM5owqnPg0MomT8GIHa2GhyrTNl0zgE7vzksFfzCqHF2Wjzyuaflw"
 }
